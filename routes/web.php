dbd16f256d214e5691ecd905d25fa6d398b5827e
sandhikagalih <sandhikagalih@gmail.com>
2025-03-29 13:20:16 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,30 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', ['title' => 'Home Page']);
+});
+
+Route::get('/posts', function () {
+    $posts = Post::all();
+    return view('posts', ['title' => 'Blog', 'posts' => $posts]);
+});
+
+Route::get('/posts/{slug}', function ($slug) {
+    $post = Post::find($slug);
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
+});
+
+Route::get('/about', function () {
+    return view('about', ['title' => 'About']);
+});
+
+Route::get('/contact', function () {
+    return view('contact', ['title' => 'Contact Us']);
 });
 
 Route::get('/dashboard', function () {
@@ -17,4 +37,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
