502c7f0248eb5b2075e534cb38b7279d578ecdaa
sandhikagalih <sandhikagalih@gmail.com>
2025-03-29 13:11:09 +0700

    buat branch baru untuk coba breeze

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,32 +2,47 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Post
+class Post extends Model
 {
-  public static function all()
-  {
-    return [
-      [
-        'id' => 1,
-        'slug' => 'judul-artikel-1',
-        'title' => 'Judul Artikel 1',
-        'author' => 'Sandhika Galih',
-        'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed magni quam eum provident expedita cumque non ducimus maxime. Quaerat commodi harum ducimus laborum ratione modi velit. Distinctio vero exercitationem esse!'
-      ],
-      [
-        'id' => 2,
-        'slug' => 'judul-artikel-2',
-        'title' => 'Judul Artikel 2',
-        'author' => 'Sandhika Galih',
-        'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi corrupti nesciunt nemo fugiat vero tempore, ducimus saepe facilis iste quasi recusandae, itaque odio aliquam doloribus! Ipsa beatae quo laborum a.'
-      ]
-    ];
-  }
+    use HasFactory;
+    protected $fillable = ['title', 'author', 'slug', 'body'];
+    protected $with = ['author', 'category'];
 
-  public static function find($slug)
-  {
-    return Arr::first(static::all(), fn($post) => $post['slug'] == $slug) ?? abort(404);
-  }
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function scopeFilter(Builder $query, array $filters): void
+    {
+        $query->when($filters['search'] ?? false, function ($query, $search) {
+            return $query->where('title', 'like', '%' . $search . '%');
+        });
+
+        $query->when($filters['category'] ?? false, function ($query, $category) {
+            return $query->whereHas(
+                'category',
+                fn(Builder $query) =>
+                $query->where('slug', $category)
+            );
+        });
+
+        $query->when($filters['author'] ?? false, function ($query, $author) {
+            return $query->whereHas(
+                'author',
+                fn(Builder $query) =>
+                $query->where('username', $author)
+            );
+        });
+    }
 }
